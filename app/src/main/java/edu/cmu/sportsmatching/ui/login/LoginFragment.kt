a22bb47f110de6568e9d7378d3e33843b1aab42f
ychu824 <yhu3@andrew.cmu.edu>
2022-03-15 01:15:45 -0400

    Test

---
+++ app/src/main/java/edu/cmu/sportsmatching/ui/login/LoginFragment.kt
@@ -0,0 +1,147 @@
+package edu.cmu.sportsmatching.ui.login
+//
+//import android.app.Activity
+//import android.content.Intent
+//import android.os.Bundle
+//import android.text.Editable
+//import android.text.TextWatcher
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.view.inputmethod.EditorInfo
+//import android.widget.EditText
+//import android.widget.Toast
+//import androidx.annotation.StringRes
+//import androidx.core.widget.doAfterTextChanged
+//import androidx.fragment.app.Fragment
+//import androidx.lifecycle.Observer
+//import androidx.lifecycle.ViewModelProvider
+//import edu.cmu.sportsmatching.MainActivity
+//import edu.cmu.sportsmatching.R
+//import edu.cmu.sportsmatching.databinding.FragmentLoginBinding
+//
+//class LoginFragment : Fragment() {
+//
+//    private lateinit var loginViewModel: LoginViewModel
+//    private lateinit var binding: FragmentLoginBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = FragmentLoginBinding.inflate(layoutInflater)
+//        val username = binding.username
+//        val password = binding.password
+//        val login = binding.login
+//        val loading = binding.loading
+//        val register = binding.register
+//
+//        loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
+//            .get(LoginViewModel::class.java)
+//
+//        loginViewModel.loginFormState.observe(this@LoginFragment, Observer {
+//            val loginState = it ?: return@Observer
+//            // disable login button unless both username / password is valid
+//            login.isEnabled = loginState.isDataValid
+//
+//            if (loginState.usernameError != null) {
+//                username.error = getString(loginState.usernameError)
+//            }
+//            if (loginState.passwordError != null) {
+//                password.error = getString(loginState.passwordError)
+//            }
+//        })
+//
+//        loginViewModel.loginResult.observe(this@LoginFragment, Observer {
+//            val loginResult = it ?: return@Observer
+//
+//            loading.visibility = View.GONE
+//            if (loginResult.error != null) {
+//                showLoginFailed(loginResult.error)
+//                return@Observer
+//            }
+//            if (loginResult.success != null) {
+//                updateUiWithUser(loginResult.success)
+//            }
+//
+//            activity?.setResult(Activity.RESULT_OK)
+//            activity?.finish()
+//
+//            // Go to main activity if succeeded
+//            val intent = Intent(activity, MainActivity::class.java)
+//            startActivity(intent)
+//        })
+//
+//        username.afterTextChanged {
+//            loginViewModel.loginDataChanged(
+//                username.text.toString(), password.text.toString()
+//            )
+//        }
+//
+//        password.apply {
+//            doAfterTextChanged {
+//                loginViewModel.loginDataChanged(
+//                    username.text.toString(), password.text.toString()
+//                )
+//            }
+//
+//            setOnEditorActionListener { _, actionId, _ ->
+//                when (actionId) {
+//                    EditorInfo.IME_ACTION_DONE ->
+//                        loginViewModel.login(
+//                            username.text.toString(), password.text.toString()
+//                        )
+//                }
+//                false
+//            }
+//
+//            login.setOnClickListener {
+//                loading.visibility = View.VISIBLE
+//                loginViewModel.login(
+//                    username.text.toString(), password.text.toString()
+//                )
+//            }
+//        }
+//
+//        register.setOnClickListener {
+//
+//        }
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+////        binding = FragmentLoginBinding.inflate(inflater, container, false)
+//        return binding.root
+//    }
+//
+//    private fun updateUiWithUser(model: LoggedInUserView) {
+//        val welcome = getString(R.string.welcome)
+//        val displayName = model.displayName
+//        // TODO : initiate successful logged in experience
+//        Toast.makeText(
+//            this.context,
+//            "$welcome $displayName",
+//            Toast.LENGTH_LONG
+//        ).show()
+//    }
+//
+//    private fun showLoginFailed(@StringRes errorString: Int) {
+//        Toast.makeText(this.context, errorString, Toast.LENGTH_SHORT).show()
+//    }
+//}
+//
+///**
+// * Extension function to simplify setting an afterTextChanged action to EditText components.
+// */
+//fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+//    this.addTextChangedListener(object : TextWatcher {
+//        override fun afterTextChanged(editable: Editable?) {
+//            afterTextChanged.invoke(editable.toString())
+//        }
+//
+//        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+//
+//        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+//    })
+//}
