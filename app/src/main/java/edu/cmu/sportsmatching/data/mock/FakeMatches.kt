995cec5aa8fb927fd7ac699406d6028d99816fd9
Alicewyz <yizhenw@andrew.cmu.edu>
2022-04-20 00:14:54 -0400

    detail_page_fragment_copied

--- app/src/main/java/edu/cmu/sportsmatching/data/mock/FakeMatches.kt
+++ app/src/main/java/edu/cmu/sportsmatching/data/mock/FakeMatches.kt
@@ -1,5 +1,6 @@
 package edu.cmu.sportsmatching.data.mock
 
+import edu.cmu.sportsmatching.data.model.Event
 import edu.cmu.sportsmatching.data.model.Match
 
 object FakeMatches {
@@ -20,5 +21,10 @@ object FakeMatches {
             imageUri = "https://cdn.vox-cdn.com/thumbor/l3P7vpY-Onaliz9OsgUz3y2mlh0=/0x0:4022x2681/920x613/filters:focal(1627x217:2269x859):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/70624496/usa_today_17896947.0.jpg", currentTeam = 4, totalTeam = 4, location = "Pittsburgh, PA, 15217",
             sport = "Table Tennis", startTime = "16:00", endTime = "18:00", date = "March 30th"
         )
+    )
+
+    val events: List<Event> = listOf(
+        Event(location = "Pittsburgh, PA, 15219", sport_type = "Basketball",start_time = "16:00", end_time = "18:00",
+            ocur_spots = 7, all_spots = 10, date = "Today")
     )
 }
