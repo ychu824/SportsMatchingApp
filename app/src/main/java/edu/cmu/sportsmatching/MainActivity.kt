ab3e62b6fbcf43dd60b2cf7c9871db481bd7d2da
ychu824 <yhu3@andrew.cmu.edu>
2022-04-23 15:14:57 -0400

    Fix UI and colors

--- app/src/main/java/edu/cmu/sportsmatching/MainActivity.kt
+++ app/src/main/java/edu/cmu/sportsmatching/MainActivity.kt
@@ -7,12 +7,10 @@ import android.widget.ListView
 import android.widget.Toast
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import edu.cmu.sportsmatching.data.mock.FakeMatches.users
 import edu.cmu.sportsmatching.databinding.ActivityMainBinding
-import edu.cmu.sportsmatching.ui.home.TeamMemberAdaptor
 import edu.cmu.sportsmatching.ui.archive.ArchiveFragment
-import edu.cmu.sportsmatching.ui.home.DetailPageFragment
 import edu.cmu.sportsmatching.ui.home.HomeFragment
+import edu.cmu.sportsmatching.ui.startmatch.StartMatchFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,14 +36,11 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG, "Bottom Navigation item is selected.")
             when (it.itemId) {
                 R.id.home -> {
-                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
-                    val fragmentManager: FragmentManager? = this?.supportFragmentManager
-                    if (fragmentManager != null) {
-                        val transaction = fragmentManager.beginTransaction()
-                        transaction.setReorderingAllowed(true)
-                        transaction.replace(R.id.main_fragment_container, HomeFragment())
-                        transaction.commit()
-                    }
+                    val fragmentManager: FragmentManager = this.supportFragmentManager
+                    val transaction = fragmentManager.beginTransaction()
+                    transaction.setReorderingAllowed(true)
+                    transaction.replace(R.id.main_fragment_container, HomeFragment())
+                    transaction.commit()
                 }
 
                 R.id.friends -> {
@@ -53,18 +48,19 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 R.id.start_match -> {
-                    Toast.makeText(this, "Start Match", Toast.LENGTH_SHORT).show()
+                    val fragmentManager: FragmentManager = this.supportFragmentManager
+                    val transaction = fragmentManager.beginTransaction()
+                    transaction.setReorderingAllowed(true)
+                    transaction.replace(R.id.main_fragment_container, StartMatchFragment())
+                    transaction.commit()
                 }
 
                 R.id.chat_room -> {
-                    Toast.makeText(this, "Archive", Toast.LENGTH_SHORT).show()
-                    val fragmentManager: FragmentManager? = this?.supportFragmentManager
-                    if (fragmentManager != null) {
-                        val transaction = fragmentManager.beginTransaction()
-                        transaction.setReorderingAllowed(true)
-                        transaction.replace(R.id.main_fragment_container, ArchiveFragment())
-                        transaction.commit()
-                    }
+                    val fragmentManager: FragmentManager = this.supportFragmentManager
+                    val transaction = fragmentManager.beginTransaction()
+                    transaction.setReorderingAllowed(true)
+                    transaction.replace(R.id.main_fragment_container, ArchiveFragment())
+                    transaction.commit()
                 }
 
                 R.id.user_profile -> {
@@ -74,11 +70,6 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
-
-//        listView = findViewById(R.id.list)
-//        val adapter = TeamMemberAdaptor(this, R.layout.team_member, users)
-//        listView.adapter = adapter
-
     }
 
 }
